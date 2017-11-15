8e60ded67c288e3c8728a2002c9448c650b6d5ae
drexler <andrew.quartey@gmail.com>
2017-11-15 16:03:00 -0500

    refactor issue-tracker api

--- src/issue-tracker/IIssueTrackerApi.ts
+++ src/issue-tracker/IIssueTrackerApi.ts
@@ -6,16 +6,17 @@
 export interface IIssueTrackerApi {
 
   /**
-   * Retrieve an issue's metadata by its identifier
+   * Retrieve an issue's metadata
+   * @param {(number|string)} alias - The unique identifier or alias for the issue.
    * @return - Issue metadata.
    */
-  getIssue(id: number): any;
+  getIssue(idOrAlias: number | string): any;
 
   /**
    * Retrieve a specified issue's comments
-   * @param {number} issueId - The unique identifier for the issue
+   * @param {(number|string)} alias - The unique identifier or alias for the issue.
    * @return - List of comments for the issue.
    */
-  getComments(issueId: number): any;
+  getComments(idOrAlias: number | string): any;
 
 }
